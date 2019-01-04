ed5a758468b5ea27e6c250235999266f321d7bf2
boolks <bootre@naver.com>
2019-01-04 16:21:05 +0900

    Create c-code157.c

---
+++ c-code/c-code157.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    printf("sizeof = %d byte\n", sizeof("program"));
+    printf("strlen = %d byte\n", strlen("program"));
+
+    return 0;
+}
